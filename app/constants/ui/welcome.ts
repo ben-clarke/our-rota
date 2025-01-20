e1eaadb7e1f47827b49a4fd259d90882158fa6bf
Ben Clarke <34235717+ben-clarke@users.noreply.github.com>
2025-01-20 20:02:04 +0000

    Making more unique

---
+++ app/constants/ui/welcome.ts
@@ -0,0 +1,6 @@
+export const GET_STARTED_NOW = "Get Started Now";
+
+export const DESCRIPTION = "Organise you rota with ease through the power of AI";
+
+export const BUTTON_GET_STARTED = "Get Started";
+export const BUTTON_SIGN_IN = "Sign In";
--- app/route-handlers/client/components/welcome/welcome.tsx
+++ app/route-handlers/client/components/welcome/welcome.tsx
@@ -1,82 +1,94 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import {
+  BUTTON_GET_STARTED,
+  BUTTON_SIGN_IN,
+  DESCRIPTION,
+  GET_STARTED_NOW,
+} from "~/constants/ui/welcome";
 
-export function Welcome({ message }: { message: string }) {
+export const Welcome = ({ message }: { message: string }) => {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img src={logoLight} alt="React Router" className="block w-full dark:hidden" />
-            <img src={logoDark} alt="React Router" className="hidden w-full dark:block" />
+      <div className="relative py-16">
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+        >
+          <div className="blur-[106px] h-56 bg-gradient-to-br from-teal-500 to-purple-400 dark:from-blue-700"></div>
+          <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+        </div>
+
+        <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+          <div className="relative">
+            <div className="flex items-center justify-center -space-x-2">
+              <img
+                loading="lazy"
+                width="400"
+                height="400"
+                src="https://randomuser.me/api/portraits/lego/0.jpg"
+                alt="member photo"
+                className="h-8 w-8 rounded-full object-cover"
+              />
+              <img
+                loading="lazy"
+                width="200"
+                height="200"
+                src="https://randomuser.me/api/portraits/lego/0.jpg"
+                alt="member photo"
+                className="h-12 w-12 rounded-full object-cover"
+              />
+              <img
+                loading="lazy"
+                width="200"
+                height="200"
+                src="https://randomuser.me/api/portraits/lego/3.jpg"
+                alt="member photo"
+                className="z-10 h-16 w-16 rounded-full object-cover"
+              />
+              <img
+                loading="lazy"
+                width="200"
+                height="200"
+                src="https://randomuser.me/api/portraits/lego/1.jpg"
+                alt="member photo"
+                className="relative h-12 w-12 rounded-full object-cover"
+              />
+              <img
+                loading="lazy"
+                width="200"
+                height="200"
+                src="https://randomuser.me/api/portraits/lego/2.jpg"
+                alt="member photo"
+                className="h-8 w-8 rounded-full object-cover"
+              />
+            </div>
+
+            <div className="mt-6 m-auto space-y-6 md:w-8/12 lg:w-7/12">
+              <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white md:text-5xl">
+                {GET_STARTED_NOW}
+              </h1>
+              <p className="text-center text-xl text-gray-600 dark:text-gray-300">{DESCRIPTION}</p>
+              <div className="flex flex-wrap justify-center gap-6">
+                <a
+                  href="#"
+                  className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:bg-teal-500 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                >
+                  <span className="relative text-base font-semibold text-white dark:text-dark">
+                    {BUTTON_GET_STARTED}
+                  </span>
+                </a>
+                <a
+                  href="#"
+                  className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-teal-500/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
+                >
+                  <span className="relative text-base font-semibold text-teal-500 dark:text-white">
+                    {BUTTON_SIGN_IN}
+                  </span>
+                </a>
+              </div>
+            </div>
           </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-              <li className="self-stretch p-3 leading-normal">{message}</li>
-            </ul>
-          </nav>
         </div>
       </div>
     </main>
   );
-}
-
-const resources = [
-  {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
-  },
-];
+};
